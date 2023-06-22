91b810cf738dab2d1b16a7cb11a848ef969e48a2
ro cant sleep <115865263+icebrrrr@users.noreply.github.com>
2023-06-21 19:38:04 -0700

    3

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-fetch("me.json").then(function (response){
-  return response.jason();
-}).then(function (obj){
-  console.log(obj);
-}).catch(function (error) {
-  console.error("oops looks like a failure (you) :3"); //either a network error or the jason file has a parcing error
-  
-})
+fetch("me.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (obj) {
+    console.log(obj);
+  })
+  .catch(function (error) {
+    console.error("Oops, looks like a failure :3 : " + error);
+  });
