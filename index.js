b9d04e3aea19bb0486abaadc68189202c977701d
ro cant sleep <115865263+icebrrrr@users.noreply.github.com>
2023-06-22 18:45:13 -0700

    5

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-fetch("me.json")//http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=DF10C6945517FC63BBDCC3B9EE3753C1&steamids=76561199092973836
+fetch("steamapi.json")//http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=DF10C6945517FC63BBDCC3B9EE3753C1&steamids=76561199092973836
   .then(function (response) {
     return response.json();
   })
-  .then(function (obj) {
-    console.log(obj);
+  .then(function (jsonObj) {
+    console.log(jsonObj);
+    console.log(jsonObj["response"]["players"][0]["personaname"]);
+    document.getElementById("username").innerHTML = (jsonObj["response"]["players"][0]["personaname"]);
   })
   .catch(function (error) {
     console.error("Oops, looks like a failure :3 : " + error);
