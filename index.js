4c431b396b1c12834bda1c393a4208efbb78e6c1
ro cant sleep <115865263+icebrrrr@users.noreply.github.com>
2023-06-28 22:54:20 -0700

    6

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("steamapi.json")//http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=DF10C6945517FC63BBDCC3B9EE3753C1&steamids=76561199092973836
+fetch("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=DF10C6945517FC63BBDCC3B9EE3753C1&steamids=76561199092973836")
   .then(function (response) {
     return response.json();
   })
